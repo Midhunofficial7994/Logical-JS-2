23b4692697bbd118be3038d8b038479ec27ca885
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-11-09 12:30:04 +0530

    Sqrt

--- 1.js
+++ 1.js
@@ -34,3 +34,39 @@
     
    
 //     return false;
+
+
+
+// Sqrt 
+
+// function mySqrt(x) {
+//     if (x === 0 || x === 1) {
+//         return x;
+//     }
+    
+//     let left = 0;
+//     let right = x;
+    
+//     while (left <= right) {
+//         let mid = Math.floor((left + right) / 2);
+//         let square = mid * mid;
+        
+//         if (square === x) {
+//             return mid; 
+//         } else if (square < x) {
+//             left = mid + 1;
+//         } else {
+//             right = mid - 1;
+//         }
+//     }
+    
+//     return right; 
+// }
+
+
+// -------------------------------------------------------
+
+// function mySqrt(x) {
+  
+//     return Math.floor(Math.pow(x, 0.5));
+// }
