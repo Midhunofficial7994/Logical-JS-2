4a32096a1aad6afb478248be36bb0e3364a907c4
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-11-09 10:14:41 +0530

    Finding words containing

---
+++ 1.js
@@ -0,0 +1,14 @@
+function findWordsContaining(words, x) {
+    let result = [];
+    
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].includes(x)) {
+            result.push(i);
+        }
+    }
+    
+    return result;
+}
+
+
+console.log(findWordsContaining(["leet","code"],"e"));
