4aac21a2b9ddb43501c5ad3e5a2a6c333a0b5dc7
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-11-09 12:28:40 +0530

    Contains duplicate

--- 1.js
+++ 1.js
@@ -1,14 +1,36 @@
-function findWordsContaining(words, x) {
-    let result = [];
+// function findWordsContaining(words, x) {
+//     let result = [];
     
-    for (let i = 0; i < words.length; i++) {
-        if (words[i].includes(x)) {
-            result.push(i);
-        }
-    }
+//     for (let i = 0; i < words.length; i++) {
+//         if (words[i].includes(x)) {
+//             result.push(i);
+//         }
+//     }
     
-    return result;
-}
+//     return result;
+// }
 
 
-console.log(findWordsContaining(["leet","code"],"e"));
+// console.log(findWordsContaining(["leet","code"],"e"));
+
+
+
+// Contains Duplicate 
+
+
+// function containsDuplicate(nums) {
+
+//     let result = new Set();
+    
+   
+//     for (let num of nums) {
+     
+//         if (result.has(num)) 
+//             return true;
+//         }
+       
+//         result.add(num);
+//     }
+    
+   
+//     return false;
