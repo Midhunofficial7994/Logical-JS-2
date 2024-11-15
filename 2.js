40f469ca48f816af400ff69754f07940749d7fa9
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-11-15 10:09:00 +0530

    Longest Common Prefix

--- 2.js
+++ 2.js
@@ -41,3 +41,25 @@ var sumOfTheDigitsOfHarshadNumber = function(x) {
         return -1
     }
 };
+
+// Longest Common prefix 
+
+function longestCommonPrefix(strs) {
+    if (strs.length === 0) return ""; 
+    
+    let prefix = strs[0]; 
+    for (let i = 1; i < strs.length; i++) {
+       
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, prefix.length - 1);  
+            if (prefix === "") return "";  
+        }
+    }
+    
+    return prefix;  
+}
+
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"])); 
+console.log(longestCommonPrefix(["dog", "racecar", "car"]));    
+
