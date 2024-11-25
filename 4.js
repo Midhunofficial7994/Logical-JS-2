aaf44877ed2920f1b5d1128e3a6f41e9ee8a61c5
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-11-25 14:51:29 +0530

    Flower Bed

--- 4.js
+++ 4.js
@@ -7,3 +7,21 @@ var missingNumber = function(nums) {
     return expectedSum - actualSum;
 };
     
+
+// Flower bed 
+var canPlaceFlowers = function(flowerbed, n) {
+    let count = 0;
+
+    for (let i=0; i<flowerbed.length; i++) {
+        let prev = flowerbed[i - 1] || 0;
+        let curr = flowerbed[i];
+        let next = flowerbed[i + 1] || 0;
+
+        if (prev === 0 && curr !== 1 && next === 0) {
+            count++;
+            flowerbed[i] = 1;
+        } 
+    }
+
+       return count >= n;
+};
