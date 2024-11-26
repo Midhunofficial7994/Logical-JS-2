cc06669c82c17ca68505662866eeae8bf959ca88
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-11-26 16:21:16 +0530

    Minimum Value to get positive step by step

--- 4.js
+++ 4.js
@@ -25,3 +25,22 @@ var canPlaceFlowers = function(flowerbed, n) {
 
        return count >= n;
 };    
+
+
+// Minimum value to get positive step by step 
+
+var minStartValue = function(nums) {
+    let output = 0;
+    let currentSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (currentSum + nums[i] < 1) {
+            output += -(currentSum + nums[i]) + 1;
+            currentSum = 1;
+        } else {
+            currentSum += nums[i];
+        }
+    }
+    
+    return output || 1;
+};
