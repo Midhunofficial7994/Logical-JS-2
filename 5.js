3e8a10fa584fe985ea9ff6b770a8af17fb28b68b
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-11-30 09:56:44 +0530

    Relative Sorted Array

--- 5.js
+++ 5.js
@@ -4,3 +4,21 @@ var busyStudent = function(startTime, endTime, queryTime) {
     return startTime.filter((t, i) => queryTime >= t &&
      queryTime <= endTime[i]).length;  
   };
+
+// Relative Sorted Array
+
+var relativeSortArray = function(arr1, arr2) {
+    arr1.sort((a,b)=>a-b);
+    let newArr=[...arr2];
+    for(let i=0;i<arr1.length;i++){
+        if(newArr.indexOf(arr1[i]) ==-1){
+            newArr.push(arr1[i]);
+        }else{
+        newArr.splice(newArr.indexOf(arr1[i]),0,arr1[i])
+        }
+    }
+    for(let i=0;i<arr2.length;i++){
+        newArr.splice(newArr.indexOf(arr2[i]),1)
+    }
+    return newArr;
+};
