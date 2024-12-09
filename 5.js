75e05ce7e399ed3673c12fc0471b4f1078b1b6a3
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-12-09 09:47:05 +0530

    Updated

--- 5.js
+++ 5.js
@@ -119,13 +119,13 @@ var isPossibleToSplit = function(nums) {
     return true;
 }; 
 
-// Find all numbers disappeared in ana Array
+// Find all numbers disappeared in an a Array
 
 var findDisappearedNumbers = function(nums) {
     let obj={}
     for(let n of nums){
         obj[n]=1
-    }
+    }     
     let arr=[]
     for(let i=1;i<=nums.length;i++){
         if(!obj[i]) arr.push(i)
